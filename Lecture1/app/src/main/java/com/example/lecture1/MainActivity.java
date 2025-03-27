02b80559e414c6fcb6abfde4cb2d41ef3984d74c
Betelgeuse <b.telgeusedev@gmail.com>
2025-03-27 09:56:03 +0100

    finished exercise 1

---
+++ Lecture1/app/src/main/java/com/example/lecture1/MainActivity.java
@@ -0,0 +1,51 @@
+// Name of the app
+package com.example.lecture1;
+
+// Stores keys and values (data) temp memory space
+import android.icu.text.DateFormat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+// Shows app in full screen
+import androidx.activity.EdgeToEdge;
+// Controls the app activities lifecycles
+import androidx.appcompat.app.AppCompatActivity;
+// Not important lol
+import androidx.core.graphics.Insets;
+// Automatically charge layout
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //R means ressource
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentDateTime = DateFormat.getTimeInstance().format(new Date());
+                textView.setText(currentDateTime);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
